706ea236a50ca0512bc787fe44ce033140f21892
Mariya Zem <alpakagold@gmail.com>
2023-11-26 22:17:11 +0300

    commit 2

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,12 +1,13 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Objects;
 
+@Entity
 @Table
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
@@ -20,6 +21,19 @@ public class User {
 
     public User() {
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName) && Objects.equals(age, user.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, lastName, age);
     }
 
     public User(String name, String lastName, Byte age) {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,37 +1,23 @@
 package jm.task.core.jdbc.util;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 //класс для подключения базы данных
 public class Util {
-    public static Connection getConnection() {
-        String dbURL = null;
-        String dbUsername = null;
-        String dbPassword = null;
+        public static final String DB_URL = "jdbc:mysql://localhost:3306/";
+        public static final String DB_USER_NAME = "root";
+        public static final String DB_PASSWORD = "";
 
-        Properties properties = new Properties();
-        try (FileInputStream fileInputStream = new FileInputStream("src/main/resources/database.properties")) {
-            properties.load(fileInputStream);
-            dbURL = properties.getProperty("url");
-            dbUsername = properties.getProperty("username");
-            dbPassword = properties.getProperty("password");
-        } catch (IOException e) {
-            e.printStackTrace();
+        public static Connection getConnection() {
+            Connection connection = null;
+            try {
+                connection = DriverManager.getConnection(DB_URL, DB_USER_NAME, DB_PASSWORD);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return connection;
         }
-
-        Connection connection = null;
-        try {
-            assert dbURL != null;
-            connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
     }
 
-}
